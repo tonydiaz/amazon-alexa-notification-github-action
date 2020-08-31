797ad9438a2dda223301f0f8a5edfea0ac0b2525
Tony Diaz <email address>
2020-08-31 15:50:12 -0400

    Clean up notification code

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const http = require("@actions/http-client");
 const auth = require("@actions/http-client/auth");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const githubSecret = core.getInput("github-secret");
     const apiKey = core.getInput("api-key");
     const apiSecret = core.getInput("api-secret");
-  
-    const octokit = new github.GitHub(githubSecret);
-    const comment = github.context.payload.comment;
+    const message = core.getInput("message");
+
     const notifyMeEndpoint = "https://api.getnotify.me/submit";
     const creds = new auth.BasicCredentialHandler(apiKey, apiSecret);
     const httpClient = new http.HttpClient('notify-me', [creds]);
-    console.log(github);
-    console.log("context", github.context);
-    console.log(github.context.eventName);
+
     const body = {
-      'message': 'hello world'
+      'message': message
     }
     let response = await httpClient.postJson(notifyMeEndpoint, body);
     console.log('response', response);

99cb0b00505e2d110e7550ed057da34a03f86c9f
Tony Diaz <email address>
2020-08-31 14:40:53 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const http = require("@actions/http-client");
+const auth = require("@actions/http-client/auth");
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const githubSecret = core.getInput("github-secret");
+    const apiKey = core.getInput("api-key");
+    const apiSecret = core.getInput("api-secret");
+  
+    const octokit = new github.GitHub(githubSecret);
+    const comment = github.context.payload.comment;
+    const notifyMeEndpoint = "https://api.getnotify.me/submit";
+    const creds = auth.BasicCredentialHandler(apiKey, apiSecret);
+    const httpClient = new http.HttpClient('notify-me', [creds]);
+    console.log(github.context);
+    const body = {
+      'message': 'hello world'
+    }
+    let response = await httpClient.postJson(notifyMeEndpoint, body);
+    console.log('response', response);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
